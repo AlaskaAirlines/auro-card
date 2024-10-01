52085257c17436bb057b48304f072f35d6c5834b
Jordan Jones <jmjones2463@gmail.com>
2024-10-01 15:11:23 -0700

    perf: use custom registered version of hyperlink #70

--- test/auro-card.test.js
+++ test/auro-card.test.js
@@ -40,15 +40,15 @@ describe('auro-card', () => {
   });
 
   it('auro-hyperlink is rendered when `href` attribute is present', async () => {
-    const el = await fixWebComponent( "/");
-    const link = el.shadowRoot.querySelectorAll('auro-hyperlink');
+    const el = await fixWebComponent("/");
+    const link = el.shadowRoot.querySelectorAll('[auro-hyperlink]');
 
     await expect(link.length).to.equal(1);
   });
 
   it('does not render auro-hyperlink when href attribute is undefined', async () => {
     const el = await fixWebComponent(undefined);
-    const link = el.shadowRoot.querySelector('auro-hyperlink');
+    const link = el.shadowRoot.querySelector('[auro-hyperlink]');
 
     expect(link).to.be.null;
   });
@@ -58,7 +58,7 @@ describe('auro-card', () => {
       <auro-card href="/auro" rel="hyperlink" role="button" target="parent"></auro-card>
     `);
 
-    const anchor = el.shadowRoot.querySelector('auro-hyperlink');
+    const anchor = el.shadowRoot.querySelector('[auro-hyperlink]');
 
     expect(anchor).to.have.attribute('rel', 'hyperlink');
     expect(anchor).to.have.attribute('role', 'button');
