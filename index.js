3c780ffb2eb5103660b32fb3d14a30018f4c206e
Eunsun Mota <eunsun.mota@alaskaair.com>
2024-10-23 12:00:35 -0700

    feat(api): add register static method #77
    - Update dependency versions
    
    - `AuroCard.register` is to easily register the element without extra importing
    - `import "@aurodesignsystem/auro-card"` will still register this element to <auro-card>
    - `import { AuroCard } from '../src/auro-card` wont register this element until `AuroCard.register` gets called
    
    - Fixed the typo on Recommended Use and Version Control: `clement` -> `element`

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 import { AuroCard } from './src/auro-card.js';
-import * as RuntimeUtils from '@aurodesignsystem/auro-library/scripts/utils/runtimeUtils.mjs';
 
-RuntimeUtils.default.prototype.registerComponent('custom-card', AuroCard);
+AuroCard.register();
