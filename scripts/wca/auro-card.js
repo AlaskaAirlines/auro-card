0dcaf8b1c7ffdad8078223f55d80476b060936e2
Joe Yang <joe.yang@alaskaair.com>
2025-04-03 14:38:39 -0700

    perf: add wca script for docs api

---
+++ scripts/wca/auro-card.js
@@ -0,0 +1,11 @@
+
+import { AuroCard } from '../../src/auro-card.js';
+
+/**
+ * The `<auro-card>` element provides users a flexible way to convey a summary of information.
+ */
+class AuroCardWCA extends AuroCard {}
+
+if (!customElements.get("auro-card")) {
+  customElements.define("auro-card", AuroCardWCA);
+}
